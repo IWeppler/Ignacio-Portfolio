f4ef5605f1cee1564f8e4ec839a661480d5ce920
IWeppler <ignacionweppler@gmail.com>
2025-07-09 17:30:56 -0300

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
+  site: "https://ignacioweppler.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
